b5491862e4f9ed2bbd415264c15e5a0edb6a037e
unknown <insooneelife@naver.com>
2020-05-17 02:36:45 +0900

    temp

--- poo/poo/source.cpp
+++ poo/poo/source.cpp
@@ -1,133 +1,173 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 #include <SFML/Graphics.hpp>
-
 using namespace std;
-using namespace sf;
 
-float GameWidth = 640;
-float GameHeight = 800;
+const float GameWidth = 640;
+const float GameHeight = 800;
 
+// github
+// AlgorFati
+
+// 0~1 
 float randomFloat()
 {
 	return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
 }
 
-float getRadiusFromSprite(const Sprite& sprite)
+class Poo
 {
-	FloatRect rect = sprite.getGlobalBounds();
+public:
+	Poo(const sf::Texture& texture)
+	{
+		_sprite.setTexture(texture);
+		_sprite.setScale(0.1f, 0.1f);
 
-	return std::min(rect.width / 2, rect.height / 2);
+		_sprite.setPosition(randomFloat() * GameWidth, randomFloat() * 100);
+
+		_velocity = sf::Vector2f(0, 3.0f + randomFloat() * 2.5f);
+
+	}
+
+	const sf::Sprite& getSprite() const { return _sprite; }
+
+	void move()
+	{
+		_velocity += sf::Vector2f(0, 0.1f + randomFloat() * 0.1f);
+
+		_sprite.move(_velocity);
+	}
+
+private:
+	sf::Sprite _sprite;
+	sf::Vector2f _velocity;
+
+};
+
+std::vector<Poo> pooList;
+int level = 0;
+
+
+float getRadiusFromSprite(const sf::Sprite& sprite)
+{
+	sf::FloatRect rect = sprite.getGlobalBounds();
+	return std::min<float>(rect.width / 2, rect.height / 2);
 }
 
-Vector2f getCenterFromSprite(const Sprite& sprite)
+sf::Vector2f getCenterFromSprite(const sf::Sprite& sprite)
 {
-	FloatRect rect = sprite.getGlobalBounds();
+	sf::FloatRect rect = sprite.getGlobalBounds();
+	float x = rect.left + rect.width / 2;
+	float y = rect.top + rect.height / 2;
 
-	return Vector2f(rect.left + rect.width / 2, rect.top + rect.height / 2);
+	return sf::Vector2f(x, y);
 }
 
-float distanceSq(Vector2f a, Vector2f b)
+float distanceSq(sf::Vector2f a, sf::Vector2f b)
 {
-	return (a.x - b.x)* (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+	return (b.x - a.x)* (b.x - a.x) + (b.y - a.y)* (b.y - a.y);
 }
 
-// collide
-bool collideSprite(const Sprite& a, const Sprite& b)
+bool collideSprite(const sf::Sprite& a, const sf::Sprite& b)
 {
 	float aRadius = getRadiusFromSprite(a);
-	float bRadius = getRadiusFromSprite(b);
+	sf::Vector2f aCenter = getCenterFromSprite(a);
 
-	Vector2f aCenter = getCenterFromSprite(a);
-	Vector2f bCenter = getCenterFromSprite(b);
+	float bRadius = getRadiusFromSprite(b);
+	sf::Vector2f bCenter = getCenterFromSprite(b);
 
 	float disSq = distanceSq(aCenter, bCenter);
 
-	return disSq < (aRadius + bRadius)* (aRadius + bRadius);
+	return disSq < (aRadius + bRadius) * (aRadius + bRadius);
 }
 
-vector<Sprite> poos;
-
-void spawnPoo(const Texture& texture)
+void spawnPoo(const sf::Texture& texture)
 {
-	if (randomFloat() < 0.1f)
-	{
-		Sprite pooSprite;
-		pooSprite.setTexture(texture);
-		pooSprite.setScale(0.1, 0.1);
-		// random pos
-		pooSprite.setPosition(randomFloat() * GameHeight, randomFloat() * 100);
+	float val = randomFloat();
 
-		poos.push_back(pooSprite);
+	if (val < 0.1f * level)
+	{
+		pooList.push_back(Poo(texture));
 	}
 }
 
 void removePoo()
 {
-	poos.erase(
-		remove_if(poos.begin(), poos.end(), [](const Sprite& poo) { return poo.getPosition().y > 700; }),
-		poos.end()
-	);
+	pooList.erase(
+		remove_if(pooList.begin(), pooList.end(), [](const Poo& poo)
+	{
+		return poo.getSprite().getPosition().y > 700;
+	}),
+		pooList.end()
+		);
 }
+
 
 int main()
 {
-	RenderWindow window(VideoMode(GameWidth, GameHeight), "poo Game");
+	sf::RenderWindow window(sf::VideoMode(GameWidth, GameHeight), "name");
+	srand((unsigned int)time(NULL));
 
-	Texture pooText;
+	sf::Texture pooText;
 	pooText.loadFromFile("poo.png");
 
-	Texture playerText;
+	sf::Texture playerText;
 	playerText.loadFromFile("player.png");
 
-	Sprite playerSprite;
+	sf::Sprite playerSprite;
 	playerSprite.setTexture(playerText);
-	playerSprite.setPosition(0, 700);
+	playerSprite.setPosition(0, GameHeight - 100);
 
+	int cnt = 300;
 	while (window.isOpen())
 	{
-		Event ev;
+		cnt++;
+		level = cnt / 300;
+
+		sf::Event ev;
 		while (window.pollEvent(ev))
 		{
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Left))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 		{
-			playerSprite.move(Vector2f(-5, 0));
+			playerSprite.move(sf::Vector2f(-5, 0));
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Right))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 		{
-			playerSprite.move(Vector2f(5, 0));
+			playerSprite.move(sf::Vector2f(5, 0));
 		}
 
 		spawnPoo(pooText);
 
-
-		for (int i = 0; i < poos.size(); ++i)
+		for (int i = 0; i < pooList.size(); ++i)
 		{
-			if (collideSprite(poos[i], playerSprite))
+			if (collideSprite(pooList[i].getSprite(), playerSprite))
 			{
 				window.close();
 			}
-		}
-
-		// move
-		for (int i = 0; i < poos.size(); ++i)
-		{
-			poos[i].move(Vector2f(0, 5));
+			pooList[i].move();
 		}
 
 		removePoo();
 
-		window.clear(Color::White);
 
-		for (int i = 0; i < poos.size(); ++i)
+		window.clear(sf::Color::White);
+
+		for (int i = 0; i < pooList.size(); ++i)
 		{
-			window.draw(poos[i]);
+			window.draw(pooList[i].getSprite());
 		}
+
 		window.draw(playerSprite);
 
 		window.display();
+
 	}
+
+	return 0;
 }
